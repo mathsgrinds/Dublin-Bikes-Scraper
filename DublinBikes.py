e7e0acce60c295b6a78b29cfe710abeeca0d205a
MathsGrinds <mathsgrinds@users.noreply.github.com>
2016-12-02 14:33:11 +0000

    Update DublinBikes.py

--- DublinBikes.py
+++ DublinBikes.py
@@ -42,8 +42,8 @@ def DublinBikes(q):
                     Updated = Soup.find("updated").text
                     Connected = Soup.find("connected").text
                     Result += Name_of_Station[Station] + ": " + str(Available) + " bike(s) available and " + str(Free) + " station(s) free" +"\n"
-                except:
-                    pass
+            except:
+                pass
     return(Result.rstrip("\n"))
 
 print DublinBikes("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,23,24,25,26,40,41,45,47,48,49,54,55,57,59,61,62,63,65,68,69,70,71,72,73,75,76,78,79,84,87,88,89,90,92,93,94,97,99,100,101")
