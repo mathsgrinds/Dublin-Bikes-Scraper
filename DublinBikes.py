959c3c9f7398120036aad05751233716354cb2a4
Stephen <stephen@mathsgrinds.ie>
2016-12-06 00:31:49 +0000

    Update
    Update

--- DublinBikes.py
+++ DublinBikes.py
@@ -20,6 +20,7 @@ def LoadDict():
 
 def GetStation(Station):
     with requests.Session() as Session:
+        Response0 = Session.get("http://www.dublinbikes.ie/All-Stations/Station-map?KeyWords="+str(Station))
         Response1 = Session.get("http://www.dublinbikes.ie/ezjscore/call/ezjsc%3A%3Atime")
         Response2 = Session.get("http://www.dublinbikes.ie/service/stationdetails/dublin/" + str(Station))
         Response3 = Session.get("http://www.dublinbikes.ie/service/stationdetails/dublin/" + str(Station))
